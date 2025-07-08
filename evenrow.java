cbeaab9d1913352d1ab5fa8948c69d882ce4789b
Suryaprakash <suryaprakashx0@gmail.com>
2025-07-08 21:54:23 +0530

    Add files via upload

---
+++ evenrow.java
@@ -0,0 +1,10 @@
+public class evenrow {
+    public static void main(String[]args){
+        System.out.println("Even numbere between 23 to 57:");
+        for(int i=23;i<=57;i++){
+            if(i%2==0){
+            System.out.print(" "+i);
+            }
+        }
+    }
+}
